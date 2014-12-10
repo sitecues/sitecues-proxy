3e4734086836ec66eb197b26f11f47fa8fbf2e90
Seth Holladay <sholladay@aisquared.com>
2014-12-10 07:20:29 -0500

    Cleanup for production use.
    Removing all traces of the reverse proxy, which we've decided to focus on later. Also moving important functions into a utilities module. Finally, meet minimum requirements for poor man's logging, so production issues aren't completely worthless.

--- source/app.js
+++ source/app.js
@@ -3,41 +3,21 @@
 
 'use strict';
 
-// Get instances of packages we depend on
+// Get instances of modules we depend on...
 // and set up internal variables...
-var portscanner = require('portscanner'), // utility to find available ports
-    http        = require('http'),        // utility to create a debugging server
-    hoxy        = require('hoxy'),        // utility to create the actual proxy
-    loadScript,  // the load script for sitecues, which the proxy will insert
-    proxy,       // represents the server to connect to for sitecues testing
-    proxyUrl,    // the absolute URL of the proxy, which cannot be known until runtime
-    contentRoot = 'http://www.example.com',
-    verbose     = process.env.VERBOSE,
-    hostname    = process.env.HOSTNAME || '127.0.0.1',
-    minPort     = 1024,  // anything less than 1024 needs sudo on *nix and OS X
-    maxPort     = 65535, // most systems will not accept anything greater than 65535
-    defaultPort = 8000,  // port for the proxy if none is provided
-    port        = sanitizePort(process.env.PORT, minPort, maxPort, defaultPort), // set the proxy port number we will attempt to use
-
-loadScript = '' +
-    '\n        <script data-provider=\"sitecues-proxy\" type=\"text/javascript\">\n'    +
-    '            // DO NOT MODIFY THIS SCRIPT WITHOUT ASSISTANCE FROM SITECUES\n' +
-    '            var sitecues = window.sitecues || {};\n\n'                       +
-    '            sitecues.config = {\n'                                           +
-    '                site_id : \'s-00000005\'\n'                                  +
-    '            };\n\n'                                                          +
-    '            (function () {\n'                                                +
-    '                var script = document.createElement(\'script\'),\n'          +
-    '                first = document.getElementsByTagName(\'script\')[0];\n'     +
-    '                sitecues.config.script_url=document.location.protocol+'      +
-                         '\'//js.dev.sitecues.com/l/s;id=\'+sitecues.config.site_id+' +
-                         '\'/v/dev/latest/js/sitecues.js\';\n'                                 +
-    '                script.type = \'text/javascript\';\n'                        +
-    '                script.async = true;\n'                                      +
-    '                script.src=sitecues.config.script_url;\n'                    +
-    '                first.parentNode.insertBefore(script, first);\n'             +
-    '            })();\n'                                                         +
-    '        </script>\n    ';
+var portscanner   = require('portscanner'), // utility to find available ports
+    hoxy          = require('hoxy'),        // utility to create the proxy
+    util          = require('./util'),      // internal project utilities
+    loadScript    = util.getLoadScript(),   // the sitecues load script, which the proxy will insert
+    proxy,        // represents the actual server to connect to for sitecues testing
+    verbose       = process.env.VERBOSE,    // truthiness flag to say that extra logging is desiraed
+    hostname      = process.env.HOSTNAME || '127.0.0.1',  // where to attach the proxy
+    minPort       = 1024,  // anything less than 1024 needs sudo on *nix and OS X
+    maxPort       = 65535, // most systems will not accept anything greater than 65535
+    defaultPort   = 8000,  // port for the proxy if none is provided
+    port          = sanitizePort(process.env.PORT, minPort, maxPort, defaultPort), // set the proxy port number we will attempt to use
+    testFlagName  = 'SITECUES.TEST', // HTTP header to send, indicating we are testing
+    testFlagValue = 'TRUE; QA PROXY'; // HTTP header value to send, indicating what kind of testing this is
 
 function sanitizePort(data, min, max, fallback) {
 
@@ -53,7 +33,7 @@ function sanitizePort(data, min, max, fallback) {
         dataInt = parseInt(data, 10), // either an integer or NaN
         result;
 
-    data = dataInt >= 0 ? dataInt : data, // use integer, if possible, otherwise leave alone for logging purposes
+    data = dataInt >= 0 ? dataInt : data; // use integer, if possible, otherwise leave alone for logging purposes
     min  = parseInt(min, 10); // NaN is fine here because of how we use > and < later
     max  = parseInt(max, 10); // NaN is fine here because of how we use > and < later
 
@@ -80,7 +60,7 @@ function sanitizePort(data, min, max, fallback) {
     }
     else {
         if (fallbackType === 'function') {
-            result = fallback();
+            result = fallback(data, min, max);
         }
         else {
             result = fallback;
@@ -104,27 +84,26 @@ function startProxy(error, foundPort) {
     var infoTimes = 0; // counter to ignore first info log from proxy
 
     if (error) {
-        console.error('Port scanner error...', error);
+        console.error(
+            new Date().toISOString(),
+            'Port scanner error...', error
+        );
     }
     else {
         if (foundPort !== port) {
             console.log(
+                new Date().toISOString(),
                 'Port ' + port + ' is not available. Using ' + foundPort +
                 ' instead, which is the next one free.'
             );
         }
-        port     = foundPort;
-        proxyUrl = 'http://' + hostname + ':' + port;
+        port = foundPort;
 
-        proxy = new hoxy.Proxy(
-            {
-                reverse: contentRoot
-            }
-        );
+        proxy = new hoxy.Proxy();
 
         proxy.log('error warn debug', function (event) {
-            console.log(event);
             console.error(
+                new Date().toISOString(),
                 event.level[0].toUpperCase() + event.level.slice(1) + ': ' +
                 event.message[0].toUpperCase() + event.message.slice(1)
             );
@@ -133,12 +112,18 @@ function startProxy(error, foundPort) {
             }
         });
         proxy.log('info', function (event) {
-            // ignore the first info log, which shows listening port (we do it ourselves)
+            // ignore the first info log, which shows the listening port
+            // and therefor duplicates something we do ourselves
             if (infoTimes > 0) {
-                console.log(event.level + ': ' + event.message);
+                console.log(
+                    new Date().toISOString(),
+                    event.level + ': ' + event.message
+                );
             }
             infoTimes = infoTimes + 1;
         });
+
+        // --- Intercepts, which can alter data in-transit between client and server...
 
         proxy.intercept(
             {
@@ -146,14 +131,10 @@ function startProxy(error, foundPort) {
                 protocol : /^https?/   // only run if using HTTP(S)
             },
             function (req, resp) {
-                console.log('proxyURL:', proxyUrl);
-                console.log('req.url:', req.url);
-                console.log('req.url.slice(1):', req.url.slice(1));
-                console.log('req.fullUrl:', req.fullUrl());
-                // if it is an absolute link, redirect traffic to the URL given in the path......
-                if (req.url.indexOf('http://') === 1 || req.url.indexOf('https://') === 1) {
-                    console.log('Truthy!');
-                    req.fullUrl(req.url.slice(1));
+                // // if it is a sitecues domain...
+                if (req.hostname.indexOf('sitecues') >= 0) {
+                    // send a header indicating that this is a test session...
+                    req.headers[testFlagName] = testFlagValue;
                 }
             }
         );
@@ -166,20 +147,6 @@ function startProxy(error, foundPort) {
                 protocol : /^https?/     // only run if using HTTP(S)
             },
             function (req, resp) {
-                // rules for images with a src attribute...
-                resp.$('img[src]').attr('src', function (index, value) {
-                    if (value.length > 0) {
-                        // bypass the proxy entirely for downloading images...
-                        return req.fullUrl() + value;
-                    }
-                });
-                // rules for anything with an href attribute...
-                resp.$('*[href]').attr('href', function (index, value) {
-                    if (value.length > 0) {
-                        // force proxying of links, etc.
-                        return proxyUrl + '/' + value;
-                    }
-                });
                 // Remove any existing sitecues load scripts, to avoid conflicts...
                 resp.$('script[data-provider="sitecues"]').remove();
                 // Inject our desired sitecues load script...
@@ -188,7 +155,10 @@ function startProxy(error, foundPort) {
         );
 
         proxy.listen(port, function () {
-            console.log('The sitecues® proxy is on port ' + port + '.');
+            console.log(
+                new Date().toISOString(),
+                'The sitecues® proxy is on port ' + port + '.'
+            );
         });
         // TODO: Get local changes to proxy.close() merged upstream,
         //       to log messages and be consistent with .listen()
