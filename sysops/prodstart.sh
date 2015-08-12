b51bf590c487aa8c79a4849ac0d737b8b471962c
Seth Holladay <me@seth-holladay.com>
2015-08-11 22:20:25 -0400

    Fix the script that runs the proxy in production.

--- sysops/prodstart.sh
+++ sysops/prodstart.sh
@@ -12,11 +12,13 @@
 pm2 stop sitecues-proxy;
 pm2 stop sitecues-rev-proxy;
 pm2 delete sitecues-proxy;
-pm2 delete sitecues-proxy;
+pm2 delete sitecues-rev-proxy;
 
 # Our proxies check the environment for a Site ID to use in the sitecues loader.
 # This ID is meant for production, to track metrics.
-SITE_ID='s-98595d91';
+export SITE_ID='s-98595d91';
+export DIRECTION='forward';
+export HOSTNAME='proxy.dev.sitecues.com';
 
 # Path where PM2 can find the CLI entry point to start proxies.
 PROXY_BIN='bin/sitecues-proxy.js';
@@ -29,5 +31,5 @@ pm2 start $PROXY_BIN --name='sitecues-proxy' --max-memory-restart=$MAX_MEM --log
 
 # Now start another proxy instance, in reverse mode.
 # Because it is the same binary, we must 'force' it to make PM2 happy.
-REVERSE=true;
+DIRECTION='reverse';
 pm2 start $PROXY_BIN --name='sitecues-rev-proxy' --force --max-memory-restart=$MAX_MEM --log='log/all-rev.log' --output='log/out-rev.log' --error='log/err-rev.log';
