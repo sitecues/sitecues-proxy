decdb7659c30aa9f05d695aa30fdb3cf073940f1
Seth Holladay <sholladay@aisquared.com>
2015-10-24 23:10:30 -0400

    Add comments and more conversion to strict ES6.

--- bin/sitecues-proxy.js
+++ bin/sitecues-proxy.js
@@ -2,10 +2,10 @@
 
 // The command line interface for the sitecues Proxy.
 
-// Use the main module in the package.json manifest to get a hold of the API.
-var sitecuesProxy = require('../'),
-    server;
+'use strict';
 
-server = sitecuesProxy.createServer();
+// Use the main module in the package.json manifest to get a hold of the API.
+const sitecuesProxy = require('../'),
+      server = sitecuesProxy.createServer();
 
 server.listen();
