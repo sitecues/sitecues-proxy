1810252afbe48767a23a9f298577ea9757cd30c1
Seth Holladay <me@seth-holladay.com>
2015-11-05 11:25:06 -0500

    Simplify request/response matching for intercepts.
    This increases performance by eliminating some unecessary pattern matching. Also shifts responsibility for logging on the listening event to the API user, aka the bin (in the typical case).

--- bin/sitecues-proxy.js
+++ bin/sitecues-proxy.js
@@ -6,6 +6,20 @@
 
 // Use the main module in the package.json manifest to get a hold of the API.
 const sitecuesProxy = require('../'),
-      server = sitecuesProxy.createServer();
+      server = sitecuesProxy.createServer(),
+      log = require('../lib/log');
 
-server.listen();
+let message = 'The sitecues\u00AE';
+
+server.listen()
+    .then(function (server) {
+
+        const state = server.state;
+
+        if (state.reverseMode) {
+            message += ' reverse';
+        }
+        message += ' proxy is on port ' + state.port + '.';
+
+        log.info(message);
+    });
