290bc189cc84a68af6aeb41778ec5743c37716ba
Seth Holladay <me@seth-holladay.com>
2016-07-12 15:52:44 -0400

    Wire up the Try It for local HTTPS
    Also fix missing dependency.

--- try-it/index.js
+++ try-it/index.js
@@ -1,15 +1,28 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const {Server} = require('hapi');
 
 class ReverseProxy extends Server {
 
     constructor(option) {
-
-        option = Object.assign({ port : 8001 }, option);
+        option = Object.assign(
+            {
+                port : 8001,
+                tls : {
+                    key  : fs.readFileSync(path.join(__dirname, 'ssl/key/localhost.key')),
+                    cert : fs.readFileSync(path.join(__dirname, 'ssl/cert/localhost.cert'))
+                }
+            },
+            option
+        );
 
         super();
-        super.connection({ port : option.port });
+        super.connection({
+            port : option.port,
+            tls  : option.tls
+        });
     }
 
     start() {
